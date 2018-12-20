8cf55654797c480902230aea50cc293aaeea9e83
lvrnjak1 <1lamsaetf>
2018-12-20 19:23:56 +0100

    metoda gradovi

--- src/ba/unsa/etf/rpr/GeografijaDAO.java
+++ src/ba/unsa/etf/rpr/GeografijaDAO.java
@@ -8,7 +8,7 @@ public class GeografijaDAO {
     private Connection con;
     //private String url;
     private Statement statement;
-    private PreparedStatement procitajGlGrad, procitajDrzavu;
+    private PreparedStatement procitajGlGrad, procitajDrzavu, procitajSveGradove;
 
     private static void initialize() throws SQLException {
         instance = new GeografijaDAO();
@@ -21,13 +21,16 @@ public class GeografijaDAO {
 
         //pripremiti upite
         procitajGlGrad = con.prepareStatement("SELECT g.naziv, g.broj_stanovnika" +
-                "FROM grad g, drzava d" +
-                "WHERE g.id = d.glavni_grad AND d.naziv = ?");
+                                                   "FROM grad g, drzava d" +
+                                                   "WHERE g.id = d.glavni_grad AND d.naziv = ?");
 
         procitajDrzavu = con.prepareStatement("SELECT id " +
                                                    "FROM drzava" +
                                                    "WHERE naziv = ?");
 
+        procitajSveGradove = con.prepareStatement("SELECT g.naziv, g.broj_stanovnika, g.drzava " +
+                                                       "FROM grad g" +
+                                                       "ORDER BY g.broj_stanovnika DESC");
 
         //ako datoteka baza.db ne postoji napuniti podacima za
         //Pariz(2.2 mil), London(8.136 mil), Bec(1.868 mil.), Manchester(510 746), Graz(283 869)
@@ -64,4 +67,34 @@ public class GeografijaDAO {
         statement.executeUpdate("DELETE FROM grad WHERE drzava = " + result.getInt(1));
         statement.executeUpdate("DELETE FROM drzava WHERE id = " + result.getInt(1));
     }
+
+    public ArrayList<Grad> gradovi() throws SQLException {
+        //cita sve gradove iz pripremljenog upita u desc poretku po broju stanovnika
+        ResultSet result = procitajSveGradove.executeQuery();
+        if(!result.next()) return null;
+
+        ArrayList<Grad> gradovi = new ArrayList<>();
+
+        //prolazi kroz procitane gradove
+        //1-naziv, 2-broj_stanovnika, 3-id drzave
+        while (result.next()){
+            statement = con.createStatement();
+            //ovim upitom dobijem naziv drzave u kojoj se nalazi grad kojeg trenutno obradjujem
+            ResultSet d = statement.executeQuery("SELECT naziv" +
+                                                      "FROM drzava" +
+                                                      "WHERE id = " + result.getInt(3));
+
+            //pozovem metodu glavni grad da dobijem gl.grad od drzave koju upit iznad vrati
+            //zatim to + ostali podaci idu u konstruktor grada
+            Grad zaDodati = new Grad(result.getString(1), result.getInt(2),
+                    new Drzava(d.getString(1), glavniGrad(d.getString(1))));
+
+            //stavlja se u array listu
+            gradovi.add(zaDodati);
+       }
+
+       return gradovi;
+    }
+
+    
 }
